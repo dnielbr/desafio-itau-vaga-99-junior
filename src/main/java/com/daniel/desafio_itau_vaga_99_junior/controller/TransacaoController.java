211c0b21fd841c8cca3bb7c1910a320302119f07
dnielbr <everaldodaniel@hotmail.com>
2026-03-15 11:45:09 -0300

    create a Controller to all endpoint waiterd in challenger: POST, DELETE and GET, and add a logger to endpoints

---
+++ src/main/java/com/daniel/desafio_itau_vaga_99_junior/controller/TransacaoController.java
@@ -0,0 +1,49 @@
+package com.daniel.desafio_itau_vaga_99_junior.controller;
+
+import com.daniel.desafio_itau_vaga_99_junior.dto.StatisticsResponse;
+import com.daniel.desafio_itau_vaga_99_junior.entites.Transacao;
+import com.daniel.desafio_itau_vaga_99_junior.service.TransacaoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/transacao")
+public class TransacaoController {
+
+    private final Logger logger = LoggerFactory.getLogger(TransacaoController.class);
+    private final TransacaoService transacaoService;
+
+    public TransacaoController(TransacaoService transacaoService) {
+        this.transacaoService = transacaoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> criarTransacao(@RequestBody Transacao transacao){
+        logger.info("Transacao iniciada com valor: {} e data: {}", transacao.getValor(), transacao.getDataHora());
+        if(transacaoService.save(transacao)) {
+            logger.info("Transacao criada com sucesso: {}", transacao);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        }
+        logger.warn("Erro ao processar a transacao: {}", transacao);
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deletarTransacoes(){
+        logger.info("Deletando todas as transações da memória.");
+        transacaoService.delete();
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<StatisticsResponse> buscarEstatisticas(){
+        logger.info("Buscando estatísticas das transações realizadas nos ultimos 1 minuto");
+        StatisticsResponse response = transacaoService.getStatistics();
+        logger.info("Totais de transações feitas nos ultimos 1 minuto: {}", response.count());
+        return ResponseEntity.ok(response);
+    }
+
+}
