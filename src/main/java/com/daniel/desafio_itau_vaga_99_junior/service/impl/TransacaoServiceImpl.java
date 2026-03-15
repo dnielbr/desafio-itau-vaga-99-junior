76b18442df3b368af6546cbbca47175b06acaac7
dnielbr <everaldodaniel@hotmail.com>
2026-03-15 13:09:51 -0300

    feat: Adiciona logger para ver a perfomace do metodo (tempo de duracao)

--- src/main/java/com/daniel/desafio_itau_vaga_99_junior/service/impl/TransacaoServiceImpl.java
+++ src/main/java/com/daniel/desafio_itau_vaga_99_junior/service/impl/TransacaoServiceImpl.java
@@ -3,6 +3,8 @@ package com.daniel.desafio_itau_vaga_99_junior.service.impl;
 import com.daniel.desafio_itau_vaga_99_junior.dto.StatisticsResponse;
 import com.daniel.desafio_itau_vaga_99_junior.entites.Transacao;
 import com.daniel.desafio_itau_vaga_99_junior.service.TransacaoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
@@ -14,6 +16,7 @@ import java.util.stream.Collectors;
 @Service
 public class TransacaoServiceImpl implements TransacaoService {
 
+    private Logger logger = LoggerFactory.getLogger(TransacaoServiceImpl.class);
     private List<Transacao> transacoes = new ArrayList<>();
 
     @Override
@@ -35,9 +38,12 @@ public class TransacaoServiceImpl implements TransacaoService {
 
         OffsetDateTime inicio = OffsetDateTime.now().minusSeconds(60);
 
+        long start = System.nanoTime();
         DoubleSummaryStatistics statistics = this.transacoes.stream().
                 filter((t) -> !t.getDataHora().isBefore(inicio))
                 .collect(Collectors.summarizingDouble(Transacao::getValor));
+        long duracao = System.nanoTime() - start;
+        logger.info("Duração para executar a busca de estatísticas foi de: {}ms", duracao/1000000 );
 
         return new StatisticsResponse(
                 statistics.getCount(),
