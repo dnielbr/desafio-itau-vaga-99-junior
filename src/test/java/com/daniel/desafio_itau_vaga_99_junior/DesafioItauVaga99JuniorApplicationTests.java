d3fd16ca1c315b319868ad442a2b5e41ab121be4
dnielbr <everaldodaniel@hotmail.com>
2026-03-15 07:28:56 -0300

    initial config

---
+++ src/test/java/com/daniel/desafio_itau_vaga_99_junior/DesafioItauVaga99JuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.desafio_itau_vaga_99_junior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauVaga99JuniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
