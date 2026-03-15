f233790712ccc8da1f6d60ef4f5c681720b95fa4
dnielbr <everaldodaniel@hotmail.com>
2026-03-15 11:41:55 -0300

    create the entity Transacao

---
+++ src/main/java/com/daniel/desafio_itau_vaga_99_junior/entites/Transacao.java
@@ -0,0 +1,34 @@
+package com.daniel.desafio_itau_vaga_99_junior.entites;
+
+import java.time.OffsetDateTime;
+
+public class Transacao {
+
+    private double valor;
+    private OffsetDateTime dataHora;
+
+    public Transacao(double valor, OffsetDateTime dataHora) {
+        this.valor = valor;
+        this.dataHora = dataHora;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public OffsetDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(OffsetDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public boolean isValid() {
+        return this.valor >= 0 && this.dataHora.isBefore(OffsetDateTime.now());
+    }
+}
