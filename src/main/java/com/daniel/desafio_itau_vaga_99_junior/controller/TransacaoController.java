4c5d8a6bccd3c68503923951ff6599315be283cc
dnielbr <everaldodaniel@hotmail.com>
2026-03-19 07:39:14 -0300

    add a RequesMapping() to controller

--- src/main/java/com/daniel/desafio_itau_vaga_99_junior/controller/TransacaoController.java
+++ src/main/java/com/daniel/desafio_itau_vaga_99_junior/controller/TransacaoController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("api")
 public class TransacaoController {
 
     private final Logger logger = LoggerFactory.getLogger(TransacaoController.class);
